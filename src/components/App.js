785d95ae2e02f6bc20a84a5397451949a5d245db
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-06-10 06:36:42 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Post from './Post';
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <div> 
+        <h1> Hello World </h1>
+        <Post title="Add commentary"/>
+      </div>
+    );
+  }
+}
